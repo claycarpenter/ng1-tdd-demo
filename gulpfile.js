e81b48fbd89191d7a7e34cd9950769f17e333619
Clay Carpenter <claycarpenter@gmail.com>
2016-06-16 00:11:38 -0400

    Basic app build and serve support.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+
+const gulp = require('gulp'),
+      browserSync = require('browser-sync');
+
+gulp.task('serve', () => {
+  browserSync.init({
+    notify: false,
+    port: 8080,
+    server: {
+      baseDir: ['app'],
+      routes: {
+        '/bower_components':'bower_components'
+      }
+    }
+  });
+
+  gulp.watch(['app/**/*.*'])
+    .on('change', browserSync.reload);
+});
+
