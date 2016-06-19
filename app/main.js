83742ecc38a59cf5cd029cba57bad634740b4231
Clay Carpenter <claycarpenter@gmail.com>
2016-06-19 11:16:09 -0400

    Adds basic contact service and mocha unit test.

--- app/main.js
+++ app/main.js
@@ -3,4 +3,12 @@
   'use strict';
 
   angular.module('AddressBook', []);
+
+  angular.module('AddressBook').service('contactService', ['$http', function($http) {
+    this.contacts = [];
+
+    $http.get('localhost:9001/contacts', function(response) {
+      console.log(`response:`, response);
+    });
+  }]);
 }());
--- test/main-spec.js
+++ test/main-spec.js
@@ -2,9 +2,22 @@
 (function() {
   'use strict';
 
+  const assert = chai.assert,
+        expect = chai.expect;
+
   describe('The address book app', function() {
-    it('should work', function() {
-      chai.assert.isArray([]);
+    describe('the contact service', function() {
+      it('should have a contacts property', function() {
+        angular.mock.module('AddressBook');
+
+        let contactService;
+        angular.mock.inject(function($injector) {
+          contactService = $injector.get('contactService');
+        });
+
+        console.log('contacts:', contactService.contacts);
+        expect(contactService.contacts).to.be.an.instanceOf(Array);
+      });
     });
   });
 
